781c06f39b109b19a62d3990d9bf64508214b87f
Ruslan Malogulko <ruslan.malogulko@gmail.com>
2020-11-11 23:45:16 +0200

    update content

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders without crashing", () => {});
+test('renders learn react link', () => {});
